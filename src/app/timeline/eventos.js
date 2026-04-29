b30615f7fb4740123726ba12b0a1268be48cb684
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2026-04-28 22:18:47 -0500

    feat: add timeline events data and associated images

--- src/app/timeline/eventos.js
+++ src/app/timeline/eventos.js
@@ -24,6 +24,30 @@
       fecha: "01/12/2018"
     },
     {
+      titulo: "Primer aniversario",
+      contenido: "nuestro primer aniversario fue un dia muy especial ya que en ese momento ya viviamos juntos decidimos ir a peregollos sin saber que se iba a convertir en nuestro lugar favorito para nuestros aniversarios🥹❤️",
+      imagen: "/img9.jpeg",
+      fecha: "01/01/2019"
+    },
+    {
+      titulo: "Segundo Aniversario",
+      contenido: "Ya habian pasado dos años juntos y por su puesto que volvimos a peregollos a celebrar nuestro aniversario, no supimos en que momento pero ya habian pasado dos años y apenas ibas a empezar a vivir como una verdadera familia a pesar de todo lo que ya habiamos pasado. 🥹❤️",
+      imagen: "/img10.jpeg",
+      fecha: "01/01/2020"
+    },
+    {
+      titulo: "Nuestro hijo",
+      contenido: "Este día nos enteramos que ibas a ser padres, para ser sincero ya llevaba tiemppo queriendo aunque no estabamos preparados para eso, pero como todo por algo se empieza, apesar de que no estabamos en el mejor momento de la relacion por las peleas pero nuestro hijo llego para que todo cambiara y efectivamente todo fue diferente fue el mejor dia para mi vida aunque en ese momento no lo parecia pero ahora que todo paso me doy cuenta que fue lo mejor que nos pudo pasar, yo le hablaba todas las noches al vientre para que se moviera todas esas noches fueron las mejores de mi vida, aunque a veces me daba miedo de que algo malo pasara pero todo salio bien. 🥹❤️",
+      imagen: "/img11.jpeg",
+      fecha: "09/01/2020"
+    },
+    {
+      titulo: "Nacimiento de nuestro hijo",
+      contenido: "Yo creo que esta va ser la historia mas larga que nunca me voy a cansar de contar porque todo fue un desastre pero a la vez todo fue perfecto. hay que empezar porque ese año estuvimos en pandemia y no podiamos salir de la casa para nada, decian que el que iba al hospital regresaba muerto, las cosas se complicaban cada vez mas pensamos hasta en contratar una partera porque tenia mucho miedo de perderte a ti o al bebe despues de 4 meses encerrados en la casa por la pandemia en junio pudimos seguir trabajando, yo no te habia comprado nada por la situacion economica de ese momento el 25 de agosto del 2020 tenia que salir a una cita para ver al bebe y de ahi tu hermana nos iba a prestar una plata para completar las cosas del bebe las cosas del parto, tu bata, toalla, primer pañal, y primer conjunto que iba a utilizar el bebe, todo eso ibas a ir a comprar con tu hermana ese dia pero fuiste al hospital y ahi te dejaron porque el bebe ya debia nacer no me dijeron nada, yo estaba trabajando y cuando llegue a la casa a almorzar estaban saliendo con la pañalera porque te habian dejado en el hospital por el parto, yo no supe donde quede empece a temblar estaba muy contento de ver al bebe pero a la vez estaba muy asustado por lo de la pandemia porque aun estaban con muchos casos en el hospital, tu hermana se fue para el hospital a dejarte las cosas porque a mi no me dejaron entrar (hubiera cambiado lo que fuera por estar ahi) cuando sali de trabajar a las 5 sali para el hospital pero no nos dejaron verte ni tampoco iba a nacer el niño porque te faltaba nivel de dilatacion entonces te dijeron que para el otro dia ya nacia mientras te hacia efecto los medicamentos, esa noche fue eterna en nuestra casa, solo pensando miles de cosas buenas y malas, al otro dia madrugue a trabajar pero no me podia concentrar de los nervios de que ya naciera y que todo saliera bien asi paso todo el dia entre alarmas cada 2 horas de que ya nacia pero nada, hasta que el 26 de agosto del 2020, pesando 2564 gramos y midiendo 52 cm a las 5:06 pm nacio nuestro hijo, yo no sabia pero me llamaron para decirme que habia nacido pero que el niño estaba en un cuarto de oxigeno porque habia habido una complicacion en el parto, cuando me llamaron para decirme eso deje todo botado y sali para el hospital sin pensar en nada mas que mi hijo estuviera bien, llegue a eso de las 6 al hospital y ya estabas bien tu y el niño tambien solo un susto que pasamos y todo salio bien, ahi salio un enfermera que en media hora me dejaban entrar se llegaron las 9 pm y no me dejaban entrar a las 9:30 pm me dejaron ingresar a verlos cuando te vi en la camilla junto al bebe mi le pertenecio a el desde ese dia soy otra persona, todo por ustedes y para ustedes. 🥹❤️",
+      imagen: "/img12.jpeg",
+      fecha: "26/08/2020"
+    },
+    {
       titulo: "Primera hizada de bandera de nuestro hijo",
       contenido: "ese dia fue muy especial para nosotros, ver a nuestro hijo tan feliz y orgulloso de su bandera fue algo que nunca olvidare, en especial porque queria estar con el en ese momento, felicitarlo abrazarlo y decirle lo mucho que lo amo y lo mucho que me siento orgullo de el, ese dia recorde los momentos que he pasado con el y todo lo que hemos logrado como familia🥹❤️",
       imagen: "/img5.jpeg",
